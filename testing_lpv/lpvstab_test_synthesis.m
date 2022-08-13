c4b90ece526d9866aee212620d30d849def35a8a
Jared Miller <jared.fr.miller@gmail.com>
2022-08-13 18:19:58 -0400

    starting to write controlled trajectory simulations

--- testing_lpv/lpvstab_test_synthesis.m
+++ testing_lpv/lpvstab_test_synthesis.m
@@ -16,3 +16,25 @@ Th_vert = [-1 -1 1 1;
 LP = lpvstab(traj);
 
 out = LP.stab(Th_vert);
+
+%% test out the interpolating controller
+[L, Nv] = size(Th_vert);
+
+
+% th = sdpvar(L, 1);
+% c = sdpvar(Nv, 1);
+% 
+% cons = [c>=0; sum(c)==1; Th_vert*c==th];
+% Pth = optimizer(cons, 0, sdpsettings('solver', 'mosek'), th, c);
+Pth = get_vertex_interp(Th_vert);
+th_test = [1; 0.4];
+
+c_test = Pth(th_test);
+
+K_test = zeros(size(out.K{1}));
+for i = 1:Nv
+    K_test = out.K{i}*c_test(i);
+end
+% cons = [c>=0; sum(c)==1; Th_vert*c==th_test];
+% sol_th = optimize(cons, 0, []);
+% th_rec_test = 
